a80ec05aa3192b5240112ddfc257536ef327efbb
Aseward1005 <anthony.seward1005@gmail.com>
2023-09-18 14:10:38 -0700

    added a distinguisher to the url path

--- backend/helloworld/urls.py
+++ backend/helloworld/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.AnthHello, name="anthony's hello world"),
+    path("anthony", views.AnthHello, name="anthony's hello world"),
 ]
--- backend/helloworld/views.py
+++ backend/helloworld/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def AnthHello(request):
     return HttpResponse(
-        '"Our name has nothing to do with the food"'  
-        '- Anthony')
+        '"Our name has nothing to do with the food" \n'  
+        '- Seaweed')
