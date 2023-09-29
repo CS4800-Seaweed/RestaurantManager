074a80b6f1bae3082d46db9a9674ec10b49880bb
Aseward1005 <anthony.seward1005@gmail.com>
2023-09-29 12:55:17 -0700

    Changed db backend to postgres with temp values

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postresql',
+        'NAME': 'restaurantdb',
+        'USER': 'managementwebsite',
+        'PASSWORD': 'centerpointe',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'TEST': {
+            'NAME': 'restaurantdbtest'
+        }
     }
 }
 
