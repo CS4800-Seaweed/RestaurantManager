6f75ecdcbf12ee1eaf36d5a3421ef55003108211
Aseward1005 <anthony.seward1005@gmail.com>
2023-09-29 14:42:47 -0700

    started inventory management app, fixed db backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postresql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'restaurantdb',
         'USER': 'managementwebsite',
         'PASSWORD': 'centerpointe',
